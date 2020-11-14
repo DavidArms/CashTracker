8d7a9c10ac778688f1bc5d31f5f5d2d2a6f0a33f
David <davidarms1237@gmail.com>
2020-11-14 11:12:42 -0600

    Using Xamarin tabbed pages template

---
+++ CashTracker/CashTracker/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using CashTracker.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace CashTracker.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
