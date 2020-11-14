314b7b79d09814d12d8170a6514b405e4081106c
David <davidarms1237@gmail.com>
2020-11-14 11:37:35 -0600

    Flyout style baybeeee

---
+++ CashTracker/CashTracker/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using CashTracker.Models;
+using CashTracker.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CashTracker.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
