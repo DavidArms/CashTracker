ba764b99cb514889fe2b116a007668efefe4f79d
David <davidarms1237@gmail.com>
2020-11-15 12:49:26 -0600

    Actually using saving the job name as the FlyoutItem name

--- CashTracker/CashTracker/ViewModels/AddJobViewModel.cs
+++ CashTracker/CashTracker/ViewModels/AddJobViewModel.cs
@@ -8,26 +8,43 @@ namespace CashTracker.ViewModels
     {
         public ICommand AddJobCommand { get; }
 
+        private string _jobName;
+        public string JobName
+        {
+            get => _jobName;
+            set
+            {
+                SetProperty(ref _jobName, value);
+                (AddJobCommand as Command).ChangeCanExecute();
+            }
+        }
+
         public AddJobViewModel()
         {
             Title = "Add Job";
-            AddJobCommand = new Command(() =>
-            {
-                var newPage = new AddStatPage();
-                var newMenu = new FlyoutItem
+            AddJobCommand = new Command(
+                execute: () =>
                 {
-                    Title = "Added Job"
-                };
-                var content = new ShellContent()
-                {
-                    Route = "TestPage",
-                    Content = newPage
-                };
-                newMenu.Items.Add(content);
-                Shell.Current.Items.Add(newMenu);
+                    var newPage = new AddStatPage();
+                    var newMenu = new FlyoutItem
+                    {
+                        Title = JobName
+                    };
+                    var content = new ShellContent()
+                    {
+                        Route = "TestPage",
+                        Content = newPage
+                    };
+                    newMenu.Items.Add(content);
+                    Shell.Current.Items.Add(newMenu);
 
-                Shell.Current.CurrentItem = content;
-            });
+                    Shell.Current.CurrentItem = content;
+                },
+                canExecute: () => !string.IsNullOrEmpty(JobName)
+            );
+
+
+
         }
     }
 }
