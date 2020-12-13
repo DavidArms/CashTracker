6bf4a2d9fec30816279985cc01c92e66d535249c
David <davidarms1237@gmail.com>
2020-12-13 13:57:48 -0600

    Creating custom entry control

---
+++ CashTracker/CashTracker.Android/CustomRenderers/FancyEntryRenderer.cs
@@ -0,0 +1,65 @@
+﻿using Android.Content;
+using Android.Graphics.Drawables;
+using CashTracker.Controls;
+using CashTracker.Droid.CustomRenderers;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(FancyEntry), typeof(FancyEntryRenderer))]
+namespace CashTracker.Droid.CustomRenderers
+{
+    /// <summary>
+    /// Custom entry renderer for Android
+    /// </summary>
+    /// <remarks>Code from https://somostechies.com/custom-entry/ </remarks>
+    public class FancyEntryRenderer : EntryRenderer
+    {
+        public FancyEntryRenderer(Context context) : base(context)
+        { }
+
+        public FancyEntry ElementV2 => Element as FancyEntry;
+
+        protected override FormsEditText CreateNativeControl()
+        {
+            var control = base.CreateNativeControl();
+            UpdateBackground(control);
+            return control;
+        }
+
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            var overrideProperties = new List<string>
+            {
+                FancyEntry.CornerRadiusProperty.PropertyName,
+                FancyEntry.BorderThicknessProperty.PropertyName,
+                FancyEntry.BorderColorProperty.PropertyName
+            };
+
+            if (overrideProperties.Contains(e.PropertyName))
+                UpdateBackground();
+
+            base.OnElementPropertyChanged(sender, e);
+        }
+
+        protected void UpdateBackground(FormsEditText control)
+        {
+            if (control == null) return;
+
+            var gd = new GradientDrawable();
+            gd.SetColor(Element.BackgroundColor.ToAndroid());
+            gd.SetCornerRadius(Context.ToPixels(ElementV2.CornerRadius));
+            gd.SetStroke((int)Context.ToPixels(ElementV2.BorderThickness), ElementV2.BorderColor.ToAndroid());
+
+            control.SetBackground(gd);
+
+            var padTop = (int)Context.ToPixels(ElementV2.Padding.Top);
+            var padBottom = (int)Context.ToPixels(ElementV2.Padding.Bottom);
+            var padLeft = (int)Context.ToPixels(ElementV2.Padding.Left);
+            var padRight = (int)Context.ToPixels(ElementV2.Padding.Right);
+
+            control.SetPadding(padLeft, padTop, padRight, padBottom);
+        }
+    }
+}
---
+++ CashTracker/CashTracker.iOS/CustomRenderers/FancyEntryRenderer.cs
@@ -0,0 +1,94 @@
+﻿using CashTracker.Controls;
+using CashTracker.iOS.CustomRenderers;
+using CoreGraphics;
+using Foundation;
+using System.Drawing;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(FancyEntry), typeof(FancyEntryRenderer))]
+namespace CashTracker.iOS.CustomRenderers
+{
+    /// <summary>
+    /// Custom entry renderer for iOS
+    /// </summary>
+    /// <remarks>Code from https://somostechies.com/custom-entry/ </remarks>
+    public sealed class FancyEntryRenderer : EntryRendererBase<UITextFieldPadding>
+    {
+        public FancyEntry ElementV2 => Element as FancyEntry;
+
+        public FancyEntryRenderer()
+        {
+            Frame = new RectangleF(0, 20, 320, 40);
+        }
+
+        protected override UITextFieldPadding CreateNativeControl()
+        {
+            var control = new UITextFieldPadding(RectangleF.Empty)
+            {
+                Padding = ElementV2.Padding,
+                BorderStyle = UITextBorderStyle.RoundedRect,
+                ClipsToBounds = true
+            };
+
+            UpdateBackground(control);
+
+            return control;
+        }
+
+        void UpdateBackground(UITextField control)
+        {
+            if (control == null)
+                return;
+
+            control.Layer.CornerRadius = ElementV2.CornerRadius;
+            control.Layer.BorderWidth = ElementV2.BorderThickness;
+            control.Layer.BorderColor = ElementV2.BorderColor.ToCGColor();
+    }
+    }
+
+    public class UITextFieldPadding : UITextField
+    {
+        public Thickness Padding { get; set; } = new Thickness(5);
+
+        public UITextFieldPadding()
+        { }
+
+        public UITextFieldPadding(NSCoder coder) : base(coder) { }
+        public UITextFieldPadding(CGRect rect) : base(rect) { }
+
+        public override CGRect TextRect(CGRect forBounds)
+        {
+            var insets = new UIEdgeInsets(
+                (float)Padding.Top,
+                (float)Padding.Left,
+                (float)Padding.Bottom,
+                (float)Padding.Right);
+
+            return insets.InsetRect(forBounds);
+        }
+
+        public override CGRect EditingRect(CGRect forBounds)
+        {
+            var insets = new UIEdgeInsets(
+                (float)Padding.Top,
+                (float)Padding.Left,
+                (float)Padding.Bottom,
+                (float)Padding.Right);
+
+            return insets.InsetRect(forBounds);
+        }
+
+        public override CGRect PlaceholderRect(CGRect forBounds)
+        {
+            var insets = new UIEdgeInsets(
+                (float)Padding.Top,
+                (float)Padding.Left,
+                (float)Padding.Bottom,
+                (float)Padding.Right);
+
+            return insets.InsetRect(forBounds);
+        }
+    }
+}
